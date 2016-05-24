c0eaee15315b82cb7fe69107d512d2d24817288c
Adam Craig and Nick Arrasate <adam@>
2016-05-24 13:34:28 -0700

    add working function for checking alarm vs current time. not refactored.

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,12 +1,4 @@
-var currentTime = moment().format('hmA');
-console.log(currentTime);
 $(document).ready(function(){
-$('#alarmset').submit(function(event){
-  event.preventDefault();
-  var hour = $('#hour').val();
-  var minute = $('#minute').val();
-  var ampm = $('.radio:checked').val();
-  var alarmTime = hour + minute + ampm;
-  console.log(alarmTime);
-});
+
+  
 });
--- js/time-interface.js
+++ js/time-interface.js
@@ -1,4 +1,28 @@
+var currentTime;
 
-$(document).ready(function(){
-  $('#time').text(moment().format('h:mm A'));
+incrementSeconds = function() {
+  currentTime = moment().format('H:mm');
+  $('#time').html(currentTime);
+  setTimeout(incrementSeconds, 1000);
+}
+
+timeCheck = function() {
+  if (currentTime === time){
+    $('.alarmDisplay').text('be alarmed!!');
+  }
+  setTimeout(timeCheck, 1000);
+}
+
+$(document).ready(function() {
+    incrementSeconds();
+    timeCheck();
+
+    $('#alarmset').submit(function(event){
+      event.preventDefault();
+      time = $('#user_time').val();
+      console.log(time);
+      console.log(currentTime);
+      $('.display').text('Alarm is set for ' + time + '.');
+    });
+
 });
